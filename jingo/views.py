b3e5d9082473ef8879ca1ac23b41390b27ddeae6
Martin Svoboda <quick@quick-VirtualBox.(none)>
2011-09-18 12:41:49 +0200

    Reverse changes from previous commit and added deprecation warning to direct_to_template function

--- jingo/views.py
+++ jingo/views.py
@@ -1,5 +1,9 @@
 import jingo
 
 
-def direct_to_template(request, template, context=None, **kwargs):
-    return jingo.render(request, template, context, **kwargs)
+def direct_to_template(request, template, **kwargs):
+    """
+    Warning: This function is deprecated, rather use template loader
+    jingo.Loader and django.shortcuts.render
+    """
+    return jingo.render(request, template, kwargs)
